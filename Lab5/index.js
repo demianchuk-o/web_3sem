3e6fd70203d75fc54bd6785d33a34cfad29729d5
demianchuk-o <demyanchukua@gmail.com>
2022-11-25 20:58:16 +0200

    task 3 done

--- Lab5/index.js
+++ Lab5/index.js
@@ -1,15 +1,17 @@
-const x_content = document.getElementById("x").innerHTML;
-const y_content = document.getElementById("y").innerHTML;
+//task1
+const item4_content = document.getElementById("item4").innerHTML;
+const item5_content = document.getElementById("item5").innerHTML;
+document.getElementById("task3").style.display = 'none';
 
-document.getElementById("x").innerHTML = y_content;
-document.getElementById("y").innerHTML = x_content;
+document.getElementById("item4").innerHTML = item5_content;
+document.getElementById("item5").innerHTML = item4_content;
 
+//task2
 function calc_ellipse() {
     let a = document.getElementById("a_input").value;
     let b = document.getElementById("b_input").value;
 
-    if (isNaN(a) || isNaN(b) ||
-        a <= 0 || b <= 0) {
+    if (isNaN(a) || isNaN(b) || a <= 0 || b <= 0) {
         document.getElementById("a_input").value = "";
         document.getElementById("b_input").value = "";
         alert("Введіть коректні значення!");
@@ -17,19 +19,32 @@ function calc_ellipse() {
     }
     const PI = 3.14159265;
     let res = PI * a * b;
-    document.getElementById("answer2").innerHTML = `Площа овала = ${res.toFixed(4)}`;
+    document.getElementById("answer2").innerHTML += `Площа овала = ${res.toFixed(4)}<br>`;
 }
 
-/*function count_words() {
-    
-    document.getElementById("answer3").innerHTML = `слів: ${text.split(" ").length()}`;
-}*/
+//task3
+function count_words() {
+    const text = document.forms["task3"]["input_text"].value;
+    return `${text.split(" ").length}`;
+}
 
-const dialog = document.getElementById('task3dialog');
-const open_dialog = document.querySelector('.open-dialog');
+const button = document.getElementById("task3button");
 
-open_dialog.addEventListener('click', () => {
-    let text = document.forms[task3][input_text].value;
-    dialog.innerHTML = `слів: ${text.split(" ").length()}`;
-    dialog.showModal();
+button.addEventListener('click', () => {
+    msg = `words=${count_words()};`;
+    alert(msg);
+    document.cookie = msg;
 })
+
+window.addEventListener('load', (event) => {
+    document.getElementById("task3").style.display = 'none';
+    if (confirm(`cookies:\n${document.cookie}\nclear cookies?`)) {
+        document.cookie = 'words=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+        document.getElementById("task3").style.display = 'block';
+    }
+    else {
+        alert('cookies weren\'t deleted.\nplease, refresh the page.');
+        document.getElementById("task3").style.display = 'block';
+    }
+});
+
